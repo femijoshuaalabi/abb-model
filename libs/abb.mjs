edf1ac555d21968e58f938e94a8675b833deba40
Alabi Joshua Olufemi <femijoshuaalabi@gmail.com>
2023-07-06 16:22:55 +0100

    'publish'

--- libs/abb.mjs
+++ libs/abb.mjs
@@ -4,6 +4,16 @@ import { NlpManager } from '../nlp/index.js';
 import { isFileExist } from '../utils/isFileExist.mjs';
 
 export default async function (question) {
+    if (typeof question == 'undefined' || question == '') {
+        console.error('What is your question?');
+        return;
+    }
+
+    if (Array.isArray(question)) {
+        console.error('Sorry, I can not accept an array');
+        return;
+    }
+
     const modelPath = path.join(process.cwd(), 'abb/model.abb');
     if (!isFileExist(modelPath)) {
         console.error('You will need to first create a Model');
